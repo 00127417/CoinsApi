e879be1a2e6beee376f30f96375c3225b787ee11
00127417 <00127417@uca.edu.sv>
2019-05-09 10:32:31 -0600

    arreglo de app.use

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/coin', coinsRouter);
+app.use('/coins', coinsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
