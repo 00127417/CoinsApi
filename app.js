f35b1220dafc4ddac498b12990d05d7ecb2b6aa1
00127417 <00127417@uca.edu.sv>
2019-05-09 10:21:48 -0600

    prueba

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', coinsRouter);
+app.use('/coin', coinsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
